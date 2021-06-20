1873a570144c21f17acbbcbd91ff022cba79f8a5
Renato Groffe <renato.groffe@yahoo.com.br>
2021-06-20 18:19:53 -0300

    .NET 6 Preview 5 + Console Application + LINQ + Zip
    .NET 6 Preview 5 + Console Application + LINQ + Zip

---
+++ ExemploLinq6/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace ExemploLinq6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*** Testes com o método Zip ***");
+            Console.WriteLine();
+
+            var numeros = new int[] { 1, 2, 3, 10, 100 };
+            var numerosRomanos = new string[] { "I", "II", "III", "X", "C" };
+            var descricoes = new string[] { "um", "dois", "três", "dez", "cem" };
+            
+            var resultados = numeros.Zip(descricoes, numerosRomanos);
+            foreach (var resultado in resultados)
+                Console.WriteLine(resultado);
+        }
+    }
+}
